791181ce3b0e4d42b9c2ee341515de5440f54795
zeinab-sheikhi <zeinab.sheykhie@gmail.com>
2025-08-16 23:15:00 +0200

    troublshooting package installation before running the actual workflow

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -xe
+MY_ENV=genomics
+
+eval "$(conda shell.bash hook)"
+printenv
+
+conda install -n base -c conda-forge mamba
+conda activate base 
+conda list
+
+mamba env create -n $MY_ENV -f environment.yaml
+conda activate $MY_ENV
+conda list
+
+snakemake --use-conda --conda-create-envs-only --cores 1 --snakefile workflow/Snakefile
